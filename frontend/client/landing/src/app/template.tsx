3f4bf9f9e4ee2b4544fab1c16ff293f83851302a
subfield <tetelainv@gmail.com>
2025-01-24 05:53:06 +0100

    backend init - 12

--- frontend/client/landing/src/app/template.tsx
+++ frontend/client/landing/src/app/template.tsx
@@ -1,35 +1,37 @@
-"use client"
+"use client";
 
 import { ThirdwebProvider } from "thirdweb/react";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import Preloader from "@/components/molecules/Loader";
-import { useSearchParams } from 'next/navigation'
 import { saveRef } from "@/functions/saveRef";
 import { refKey } from "@/config";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   const [isClient, setIsClient] = useState(false);
-  const searchParams = useSearchParams();
 
-  const saveRefHandler = async (ref: string) => {
+  const SaveRef = async (ref: string) => {
     const { data, status } = await saveRef(ref);
     if (status === 200) {
+      console.log({ data });
       localStorage.setItem(refKey, data);
     }
   };
 
   useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  useEffect(() => {
     if (isClient) {
-      const ref = searchParams.get('ref');
+      const params = new URLSearchParams(window.location.search); // Use URLSearchParams
+      const ref = params.get("ref");
+
       if (ref) {
-        saveRefHandler(ref);
+        console.log(ref);
+        SaveRef(ref as string);
       }
     }
-  }, [isClient, searchParams]);
+  }, [isClient]);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   if (!isClient) {
     return <Preloader />;
@@ -37,9 +39,7 @@ export default function Template({ children }: { children: React.ReactNode }) {
 
   return (
     <div>
-      <ThirdwebProvider>
-        {children}
-      </ThirdwebProvider>
+      <ThirdwebProvider>{children}</ThirdwebProvider>
     </div>
   );
 }
