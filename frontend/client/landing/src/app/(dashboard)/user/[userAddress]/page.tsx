dabef07a06d193c87a334173bda8f1dbb0cf72ec
subfield <tetelainv@gmail.com>
2025-01-25 23:02:10 +0100

    fixed routing landing to dashboard

--- frontend/client/landing/src/app/(dashboard)/user/[userAddress]/page.tsx
+++ frontend/client/landing/src/app/(dashboard)/user/[userAddress]/page.tsx
@@ -1,4 +1,4 @@
-import type { Metadata, ResolvingMetadata } from 'next'
+import type { Metadata } from 'next'
 import UserIndex from "@/components/pages/Users/Index"
 import { shortenHexStringServer } from '@/utils/server'
 
