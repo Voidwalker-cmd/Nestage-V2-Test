d1ccc2c8f9d1197dab75bf360323a6504241dd92
subfield <tetelainv@gmail.com>
2025-02-01 10:54:13 +0100

    Fix minor code formatting inconsistency in DashboardNav
    Added a trailing space for better readability and consistency. No functional changes introduced, purely a code style adjustment to maintain clean codebase practices.
    
    Signed-off-by: subfield <tetelainv@gmail.com>

--- frontend/client/landing/src/components/organisms/DashboardNav.tsx
+++ frontend/client/landing/src/components/organisms/DashboardNav.tsx
@@ -8,7 +8,7 @@ const DashboardNav = () => {
   return (
     <div className="fixed lg:sticky top-0 w-full bg-white dark:bg-[#18181b] px-5 shadow !z-50">
         <div className="flex items-center justify-between py-4">
-          <div className="space-x-3 flex items-center">
+          <div className="space-x-3 flex items-center ">
             <SidebarTrigger className="!w-6 !h-6 lg:!w-5 lg:!h-5 !text-black dark:!text-white" />
             <p className="darkModeText hidden lg:block ml-5 font-semibold text-lg"><Points/></p>
           </div>
